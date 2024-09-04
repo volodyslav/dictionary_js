595add8caf14325693f901e8209bdb70f54e4b83
Vladyslav <vladver1211@gmail.com>
2024-09-04 17:50:12 +0300

    add: audio, text, difinition

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 // 
 const searchButton = document.querySelector("#searchButton");
 const searchInput = document.querySelector("#searchInput");
-const dictionaryContainer = document.querySelector("#dictionaryContainer");
+const main = document.querySelector("main");
 
+const dictionary = document.createElement("div");
+main.appendChild(dictionary);
 
 searchButton.addEventListener("click", handleSearch);
 
 function handleSearch() {
+    // Clear any existing dictionary before creating a new one.
+    dictionary.innerHTML = ""
+    main.removeChild(dictionary);
+
     const value = searchInput.value.trim().toLowerCase()
     console.log(value)
-    if (value.length > 1){
+    if (value.length > 1){ // Check size of the input
         loadWord(value);
+        dictionary.classList.add("dictionaryContainer");
+        main.appendChild(dictionary)
     }
 }
 
@@ -21,9 +29,57 @@ async function loadWord(word){
             throw new Error('Network response was not ok');
         }
         const data = await response.json(); 
-        console.log(data); 
-
+        console.log(data);
+        showResult(data); 
+        // Clear the search input field after the search is done.  //  Added this line to clear the search input field after the search is done.  //
+        searchInput.value = "" 
     }catch(e){
         console.error('An error occurred:', e);
     }
+}
+
+function showResult(result){
+    showWord(result);
+    const meaningDiv = document.createElement("div");
+    meaningDiv.classList.add("meaningsContainer");  //  Added this line to add a class to the div that contains the meanings.  //
+    for (const meaning of result[0].meanings){
+        const mean = document.createElement("p");
+        mean.textContent = meaning.partOfSpeech + ": " + meaning.definitions[0].definition;
+        mean.classList.add("meanings"); 
+        meaningDiv.appendChild(mean);
+    }
+    dictionary.appendChild(meaningDiv);
+}
+
+// This function is responsible for creating and populating the HTML elements to display the word and its phonetics.
+function showWord(result){
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("wordContainer"); // Added this line to
+
+    const wordDiv = document.createElement("h1");
+    const phoneticsDiv = document.createElement("div");
+
+    wordDiv.textContent = result[0].word;
+    wordDiv.classList.add("word-title");
+
+    phoneticsDiv.classList.add("phonetic"); // Added
+
+    for (const phonetic of result[0].phonetics){
+        if (phonetic.audio && phonetic.text){
+            const audioEl = document.createElement("audio");
+            audioEl.src = phonetic.audio; // Set the source of the audio element
+            audioEl.controls = true;
+
+            const textEl = document.createElement("p");
+            textEl.textContent = phonetic.text;
+
+            phoneticsDiv.appendChild(textEl);
+            phoneticsDiv.appendChild(audioEl);  
+            break; // If find only one audio element
+        }
+    }
+
+    mainDiv.appendChild(wordDiv);
+    mainDiv.appendChild(phoneticsDiv);
+    dictionary.appendChild(mainDiv);
 }
