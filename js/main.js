0fc4b05be2a9c335da76f8e1b083c0e518403121
Vladyslav <vladver1211@gmail.com>
2024-09-05 19:02:56 +0300

    change: structure

--- js/main.js
+++ js/main.js
@@ -13,10 +13,11 @@ function handleSearch() {
     dictionary.innerHTML = ""
     main.removeChild(dictionary);
 
-    const value = searchInput.value.trim().toLowerCase()
-    console.log(value)
+    const value = searchInput.value.trim().toLowerCase();
+    
+    console.log(value);
     if (value.length > 1){ // Check size of the input
-        loadWord(value);
+        loadWord(value); // value
         dictionary.classList.add("dictionaryContainer");
         main.appendChild(dictionary)
     }
@@ -38,55 +39,43 @@ async function loadWord(word){
     }
 }
 
-function showResult(result){
-    showWord(result);
-    const meaningDiv = document.createElement("div");
-    meaningDiv.classList.add("meaningsContainer");  //  Added this line to add a class to the div that contains the meanings.  //
-    for (const meaning of result[0].meanings){
-        const meanText = document.createElement("p");
-        meanText.textContent = meaning.partOfSpeech + ":";
-        meanText.classList.add("meanings"); 
-        meaningDiv.appendChild(meanText);
-        for (const word of meaning.definitions){
-            const wordText = document.createElement("p");
-            wordText.textContent = "- " + word.definition;
-            meaningDiv.appendChild(wordText);
-        }
-       
+function showResult(results){
+    for (const result of results) {
+        const resultDiv = document.createElement("div"); // For all words meannings
+        resultDiv.classList.add("resultContainer"); //  first iteration
         
-    }
-    dictionary.appendChild(meaningDiv);
-}
+        // word and phonetics
+        const firstDiv = document.createElement("div"); // word and phonetics
+        firstDiv.classList.add("firstDiv"); // first iteration
 
-// This function is responsible for creating and populating the HTML elements to display the word and its phonetics.
-function showWord(result){
-    const mainDiv = document.createElement("div");
-    mainDiv.classList.add("wordContainer"); // Added this line to
+        const resultWord = document.createElement("h1"); // word
+        resultWord.classList.add("resultWord");
+        resultWord.textContent = result.word;
 
-    const wordDiv = document.createElement("h1");
-    const phoneticsDiv = document.createElement("div");
+        const resultPhonetics = document.createElement("div"); // phonetics
+        resultPhonetics.classList.add("resultPhonetics"); // first iteration
+        for (const phonetic of result.phonetics){
+            if (phonetic.audio && phonetic.text){
+                const phoneticText = document.createElement("p"); // phonetic text
+                const audioEl = document.createElement("audio");
+                audioEl.src = phonetic.audio; // Set the source of the audio element
+                audioEl.controls = true;
+                phoneticText.textContent = phonetic.text;
 
-    wordDiv.textContent = result[0].word;
-    wordDiv.classList.add("word-title");
-
-    phoneticsDiv.classList.add("phonetic"); // Added
-
-    for (const phonetic of result[0].phonetics){
-        if (phonetic.audio && phonetic.text){
-            const audioEl = document.createElement("audio");
-            audioEl.src = phonetic.audio; // Set the source of the audio element
-            audioEl.controls = true;
-
-            const textEl = document.createElement("p");
-            textEl.textContent = phonetic.text;
-
-            phoneticsDiv.appendChild(textEl);
-            phoneticsDiv.appendChild(audioEl);  
-            break; // If find only one audio element
+                resultPhonetics.appendChild(audioEl);
+                resultPhonetics.appendChild(phoneticText);   
+            }
+            
         }
-    }
+        firstDiv.appendChild(resultWord);
+        firstDiv.appendChild(resultPhonetics);
+        
 
-    mainDiv.appendChild(wordDiv);
-    mainDiv.appendChild(phoneticsDiv);
-    dictionary.appendChild(mainDiv);
+        
+
+        // append in div main
+        resultDiv.appendChild(firstDiv);
+        dictionary.appendChild(resultDiv);
+    }
 }
+
